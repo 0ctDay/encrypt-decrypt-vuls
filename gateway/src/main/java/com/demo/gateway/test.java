2ea6e25fb3307219e0a742cd2e1b1364c9d0da32
Administrator <zyb2514@126.com>
2024-05-10 23:05:13 +0800

    new

--- gateway/src/main/java/com/demo/gateway/test.java
+++ gateway/src/main/java/com/demo/gateway/test.java
@@ -4,7 +4,7 @@ import com.demo.gateway.utils.AESUtil;
 
 public class test {
     public static void main(String[] args) {
-        String str = "uMDI+g2bagWZ6+wlwBGQYjPFu1pJ8oA39/HzrpZT7nbUDdZogctMTxiPY //vF2stkEFLwjhiJTlPZObyLKInxjIzojhVH3gIMVQ0mcbmp3fNbYVds8cN9jkSYxEgp1eWCXbPCDxKsTuXD6q671vlNGg2m7Qu/rW+2jelZWGYVse2KI6rLfkB54r23ZVHOU2gNEC6DYIvn2+vdtOe89pd7hM1XbRyBHYVjwM/0TTHrOU4k8d0oP5/vbmLHFUgYpXq4R4ZxsXa70+/w9xAjRU+THnItxiLx2HZgCros+F2I5uT0a4lBXtOZVUGYJe2PX08oi2ySmrgVrBYQhWX4z7/nW+0DCbBbOcT2rS9zWflFqUKOseKRRVrSE02Zk6+RgfX9yn/BTbhPjJezI9NdbZYQ==";
+        String str = "0uMDI+g2bagWZ6+wlwBGQYjPFu1pJ8oA39/HzrpZT7nbUDdZogctMTxiPY//vF2stkEFLwjhiJTlPZObyLKInxjIzojhVH3gIMVQ0mcbmp3fNbYVds8cN9jkSYxEgp1e90mT1WH+BGUSsET6ZQd0iXnxXuie7GXhfTHb92NpSGjCtag/H4uXl0D6b2a5SRNUzfweu3O5A7sgO2sAO0i+4d6MNDFBZrTos1G4E4nT6McEGytLFCybV05lapbpY2XwhWoDG1cZOiUFnnxhaAOruvSyi0A3jaz0CZYSEDHEYtzILLEQVLR++cG83Sh4T8UK1aKERw/uV9k/Z6qxtB+4AjtagvGpMyNEr94QkJvX2L9AA3K3hZ96YQFlpD0NxLF99mFcFlAXEVhN3V7PJTqKeA==";
         //String encrypt = AESUtil.encrypt(str, "MTIzNDU2Nzg5MTIzNDU2Nw==");
         String decrypt = AESUtil.decrypt(str, "MTIzNDU2Nzg5MTIzNDU2Nw==");
         System.out.println(decrypt);
