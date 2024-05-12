dfe24ac601c63ae2169cd3399e34b184d122192c
Administrator <zyb2514@126.com>
2024-05-12 23:58:05 +0800

    new

---
+++ gateway/src/main/java/com/demo/gateway/config/CustomErrorWebExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.demo.gateway.config;
+
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CustomErrorWebExceptionHandler {
+
+    @Bean
+    public ErrorWebExceptionHandler errorWebExceptionHandler(ErrorAttributes errorAttributes) {
+        return (exchange, ex) -> {
+            if (ex instanceof ResponseStatusException) {
+                ResponseStatusException responseStatusException = (ResponseStatusException) ex;
+                exchange.getResponse().setStatusCode(responseStatusException.getStatus());
+                return exchange.getResponse().setComplete();
+            }
+            exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+            return exchange.getResponse().setComplete();
+        };
+    }
+}
