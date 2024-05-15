54a5c769615c8b398c9b8ac0f4a38d8d31131416
Administrator <zyb2514@126.com>
2024-05-15 22:03:05 +0800

    github

--- gateway/src/main/java/com/demo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/demo/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package com.demo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Description:
@@ -11,7 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @Date: 2021/10/24 23:34
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+
 public class GatewayApplication {
 
     public static void main(String[] args) {
--- library/src/main/java/com/example/library/LibraryApplication.java
+++ library/src/main/java/com/example/library/LibraryApplication.java
@@ -3,11 +3,10 @@ package com.example.library;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @MapperScan("com.example.library.mapper")
 public class LibraryApplication {
 
